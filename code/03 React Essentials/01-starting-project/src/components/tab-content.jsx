5d0abccd480d5e75050eda148716ec51c3d592d8
owenkl21 <owenkl16@icloud.com>
2023-12-18 23:08:21 +0200

    Added Component
    Added more components to make the app.jsx more sleek

---
+++ code/03 React Essentials/01-starting-project/src/components/tab-content.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useState } from 'react';
+import { EXAMPLES } from '../data';
+import TabButton from '../components/TabButtons.jsx';
+
+export default function TabContent() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <>
+      <h3> Examples </h3>
+      <menu>
+        <TabButton selectedTopic={selectedTopic} onSelect={handleClick} />
+      </menu>
+      <div id="tab-content">
+        {selectedTopic ? (
+          <div>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <hr />
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code id="glow">{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        ) : (
+          <div>
+            <h3>Info</h3>
+            <p>
+              React is a popular JavaScript library for building user
+              interfaces, particularly single-page applications. It's known for
+              its component-based architecture, enabling developers to create
+              reusable UI components. React uses a virtual DOM to optimize
+              rendering and improve app performance. It also implements a
+              unidirectional data flow, providing a clear and predictable way to
+              manage application state.
+              <br />
+              <br />
+              React's declarative approach makes it straightforward to design
+              interactive UIs. Developers define the desired state of their UI,
+              and React efficiently updates and renders the right components
+              when data changes. <br />
+              <br />
+              To delve deeper, and explore more click on the topics like
+              Components, JSX, Props, and State above, which are fundamental to
+              mastering React development.
+            </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
