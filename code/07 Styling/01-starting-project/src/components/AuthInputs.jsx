3817914bcc32dc5245f4d48121456b44c8bdbc52
owenkl21 <owenkl16@icloud.com>
2024-01-10 15:05:24 +0200

    Tailwind Styles
    Added a few tailwind styles to
    the page

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -22,34 +22,45 @@ export default function AuthInputs() {
 
   return (
     <>
-      <div id="auth-inputs">
-        <div className="controls">
-          <p>
-            <label>Email</label>
+      <div
+        id="auth-inputs"
+        className="flex flex-col justify-center items-center aligntext-center max-w-md  m-auto p-8 rounded-md bg-zinc-800"
+      >
+        <div className="controls p-2 ">
+          <div className="flex flex-col mb-4">
+            <label className="mb-2 text-left font-medium text-gray-400">
+              Email
+            </label>
             <input
+              className={`bg-gray-400 w-60 h-8 rounded-md text-white p-2 font-semibold text-zinc-800 focus:ring-yellow-500 focus:border-yellow-500  focus-1`}
               type="email"
-              className={emailNotValid ? 'invalid' : undefined}
               onChange={(event) =>
                 handleInputChange('email', event.target.value)
               }
             />
-          </p>
-          <p>
-            <label>Password</label>
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-2 text-left font-medium text-gray-400 p-1">
+              Password
+            </label>
             <input
+              className={`bg-gray-400 w-60 h-8 rounded-md text-white p-2 font-semibold text-zinc-800 border-yellow-500`}
               type="password"
-              className={passwordNotValid ? 'invalid' : undefined}
               onChange={(event) =>
                 handleInputChange('password', event.target.value)
               }
             />
-          </p>
+          </div>
         </div>
-        <div className="actions">
-          <button type="button" className="text-button">
+        <div className="actions mt-4 justify-around space-x-4">
+          <button type="button" className="text-yellow-500 mr-2">
             Create a new account
           </button>
-          <button className="button" onClick={handleLogin}>
+          <span className="text-yellow-500">| </span>
+          <button
+            className="text-zinc-800 bg-yellow-500 p-3 uppercase font-medium rounded-md"
+            onClick={handleLogin}
+          >
             Sign In
           </button>
         </div>
