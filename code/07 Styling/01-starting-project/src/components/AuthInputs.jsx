811d23580bc9abbede12dd68329e60c82255e8ef
owenkl21 <owenkl16@icloud.com>
2024-01-10 14:17:02 +0200

    Added Tailwind CSS
    added the tailwind package and configs

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -21,33 +21,39 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+    <>
+      <div id="auth-inputs">
+        <div className="controls">
+          <p>
+            <label>Email</label>
+            <input
+              type="email"
+              className={emailNotValid ? 'invalid' : undefined}
+              onChange={(event) =>
+                handleInputChange('email', event.target.value)
+              }
+            />
+          </p>
+          <p>
+            <label>Password</label>
+            <input
+              type="password"
+              className={passwordNotValid ? 'invalid' : undefined}
+              onChange={(event) =>
+                handleInputChange('password', event.target.value)
+              }
+            />
+          </p>
+        </div>
+        <div className="actions">
+          <button type="button" className="text-button">
+            Create a new account
+          </button>
+          <button className="button" onClick={handleLogin}>
+            Sign In
+          </button>
+        </div>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
-      </div>
-    </div>
+    </>
   );
 }
